8ca75205d53a67ba94cfe7b426e71f2e9387f9c8
douhimed <mr.douhi.med@gmail.com>
2019-03-24 16:21:40 +0100

    adding some models

---
+++ asp-mvc/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using asp_mvc.Models;
+
+namespace asp_mvc.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        public IList<Movie> movies = new List<Movie>
+        {
+            new Movie(1, "WARRIOR"),
+            new Movie(2, "INCEPTION"),
+            new Movie(3, "WOLF ALL WALL STREET"),
+            new Movie(4, "GLADIATOR"),
+            new Movie(5, "8 miles"),
+        };
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(movies[id]);
+        }
+    }
+}
